e7cdd4a8921ae086576ba98fa69399510219dc73
jstpierre2906 <jstpierre2906@outlook.com>
2024-09-19 21:53:14 -0400

    Tests with JSX, TSX and HTML files

---
+++ src/examples/MyComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent = () => (
+  <div className="container">
+    <h1>Hello, World!</h1>
+    <p>This is a sample JSX snippet.</p>
+    <button onClick={() => alert("Button clicked!")}>Click Me</button>
+  </div>
+);
+
+export default MyComponent;
---
+++ src/transformers/html-transformer.js
@@ -0,0 +1,22 @@
+const htmlTransformer = (fileInfo, api, _options) => {
+  const j = api.jscodeshift;
+  // Abstract Syntax Tree, a.k.a ast
+  const ast = j(fileInfo.source);
+  return ast
+    .find(j.JSXElement, {
+      openingElement: {
+        name: {
+          name: "div",
+        },
+      },
+    })
+    .forEach((path) => {
+      path.node.openingElement.name.name = "section";
+      if (path.node.closingElement) {
+        path.node.closingElement.name.name = "section";
+      }
+    })
+    .toSource();
+};
+
+module.exports = htmlTransformer;
