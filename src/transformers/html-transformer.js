d4d31a03a0d67822fc96550cea48c029f2654c5e
jstpierre2906 <jstpierre2906@outlook.com>
2024-09-23 16:26:45 -0400

    Success on HTML codemod without jscodeshift (posthtml-parser, -render)

--- src/transformers/html-transformer.js
+++ src/transformers/html-transformer.js
@@ -1,24 +1,27 @@
 const fs = require("node:fs");
-const path = require("node:path");
 const { parser } = require("posthtml-parser");
-const jscodeshift = require("jscodeshift");
+const { render } = require("posthtml-render");
 
 const htmlFile = fs.readFileSync(`${__dirname}/../examples/html/example-markup.html`, "utf-8");
-const ast = parser(htmlFile)[0];
-console.log(ast); // { tag: 'div', attrs: { class: 'red' }, content: [ 'Just a div' ] }
+// Original HTML:
+// <div class="red">Just a div</div>
+const html = render(
+  ((ast) => {
+    console.log(ast);
+    // Parsed HMTL as AST:
+    //   [
+    //     { tag: 'div', attrs: { class: 'red' }, content: [ 'Just a div' ] },
+    //     '\n'
+    //   ]
+    ast
+      .filter((node) => node.tag === "div")
+      .forEach((node) => {
+        node.tag = "section";
+      });
+    return ast;
+  })(parser(htmlFile))
+);
 
-const transformAST = () => {
-  const j = jscodeshift; //.withParser("recast");
-  // jscodeshift complains about receiving an unexpected value [object Object]
-  // The AST might not be formed to what jscodeshift expects
-  // Maybe doing manual transforms with vanilla JS is the way to go, since
-  // the transforms themselves are done that way.
-  //
-  // Edit: since we have to rebuild the HTML after transformation,
-  // we have to somehow get the AST accepted by jscodeshift.
-  const root = j(ast);
-  return root.toSource();
-};
-
-const transformed = transformAST();
-console.log(transformed);
+console.log(html);
+// Modified HTML:
+// <section class="red">Just a div</section>
