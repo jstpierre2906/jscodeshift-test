d62769f6ac8e1d64f5d020b017b37378e8d43141
jstpierre2906 <jstpierre2906@outlook.com>
2024-09-28 11:50:03 -0400

    node target transformer

--- src/transformers/html-transformer.js
+++ src/transformers/html-transformer.js
@@ -6,32 +6,29 @@ const { render: postHTMLRender } = require("posthtml-render");
 // <div class="red">Just a div</div>
 const markup = fs.readFileSync(`${__dirname}/../examples/html/example-markup.html`, "utf-8");
 const modifiedHTML = postHTMLRender(
-  (({ ast, newTextContent, nodeTargetPredicate }) => {
+  (({ ast, nodeTargetPredicate, nodeTargetTransformer }) => {
     console.log(ast);
     // Parsed HMTL as AST:
     //   [
     //     { tag: 'div', attrs: { class: 'red' }, content: [ 'Just a div' ] },
     //     '\n'
     //   ]
-    ast
-      .filter((node) => nodeTargetPredicate(node))
-      .forEach((node) => {
-        node.tag = "section";
-        node.attrs.class = "blue";
-        node.content[0] = {
-          tag: "span",
-          attrs: { class: "some-stylin" },
-          content: newTextContent,
-        };
-      });
+    ast.filter((node) => nodeTargetPredicate(node)).forEach((node) => nodeTargetTransformer(node));
     return ast;
   })({
     ast: postHTMLParser(markup),
-    newTextContent: "Works!",
     nodeTargetPredicate: (node) => node.tag === "div" && node.attrs.class === "red",
+    nodeTargetTransformer: (node) => {
+      node.tag = "section";
+      node.attrs.class = "blue";
+      node.content[0] = {
+        tag: "span",
+        attrs: { class: "some-stylin" },
+        content: "works!",
+      };
+    },
   })
 );
-
 console.log(modifiedHTML);
 // Modified HTML:
 // <section class="blue"><span class="some-stylin">Works!</span></section>
