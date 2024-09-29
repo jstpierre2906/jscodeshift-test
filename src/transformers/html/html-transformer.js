462c867dcdbafd0c46db61c7cf9c7b741713b3f9
jstpierre2906 <jstpierre2906@outlook.com>
2024-09-29 12:51:44 -0400

    going modular, part 4

--- src/transformers/html/html-transformer.js
+++ src/transformers/html/html-transformer.js
@@ -1,13 +1,27 @@
-const recurseThroughHTMLTree = require("./utils/recurse.js");
-
 const htmlTransformer = ({ ast, nodeTargetPredicate, nodeTargetTransformer }) => {
-  ast.forEach((firstLevelNode) =>
-    recurseThroughHTMLTree({
-      node: firstLevelNode,
-      nodeTargetPredicate,
-      nodeTargetTransformer,
-    })
-  );
+  const attemptTransform = ({ node }) => {
+    try {
+      nodeTargetTransformer(node);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const recurseThroughHTMLTree = ({ node }) => {
+    if (node.tag && node.content) {
+      if (nodeTargetPredicate(node)) {
+        attemptTransform({ node });
+      }
+      if (Array.isArray(node.content)) {
+        node.content.forEach((nodeContentItem) => {
+          if (nodeTargetPredicate(nodeContentItem)) {
+            attemptTransform({ node: nodeContentItem });
+          }
+          recurseThroughHTMLTree({ node: nodeContentItem });
+        });
+      }
+    }
+  };
+  ast.forEach((firstLevelNode) => recurseThroughHTMLTree({ node: firstLevelNode }));
   return ast;
 };
 
--- src/transformers/html/utils/transform.js
+++
@@ -1,9 +0,0 @@
-const attemptTransform = ({ node, nodeTargetTransformer }) => {
-  try {
-    nodeTargetTransformer(node);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-module.exports = attemptTransform;
