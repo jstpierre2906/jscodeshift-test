7b762464779bc39200ff357dfded42a82cf80662
jstpierre2906 <jstpierre2906@outlook.com>
2024-10-02 21:43:30 -0400

    poc for node removal without modifying html transformer

--- src/transformers/html/main.js
+++ src/transformers/html/main.js
@@ -27,6 +27,23 @@ const transformedAST = htmlTransformer({
         node.content[0] = "posthtml works like a charm!";
       },
     },
+    {
+      nodeTargetPredicate: (node) => node.tag === "head",
+      nodeTargetTransformer: (node) => {
+        const foundIndex = node.content.findIndex(
+          (contentItem) => contentItem?.tag === "meta" && contentItem?.attrs?.name === "viewport"
+        );
+        if (foundIndex !== -1) {
+          node.content = node.content.filter((_contentItem, index) => {
+            // Also removing trailing carriage return, i.e. not leaving an empty line after node removal.
+            if (/^\n\s+$/.test(node.content[foundIndex + 1])) {
+              return index !== foundIndex && index !== foundIndex + 1;
+            }
+            return index !== foundIndex;
+          });
+        }
+      },
+    },
   ],
 });
 const modifiedHTML = postHTMLRender(transformedAST, { closingSingleTag: "slash" });
