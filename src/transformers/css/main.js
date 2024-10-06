52b8dde2422a0779814e7c858e1a1eb95dfe57c6
jstpierre2906 <jstpierre2906@outlook.com>
2024-10-06 11:08:26 -0400

    added first-level transformer

--- src/transformers/css/main.js
+++ src/transformers/css/main.js
@@ -4,13 +4,13 @@ const fs = require("node:fs");
 // const { render: postHTMLRender } = require("posthtml-render");
 const postcss = require("postcss");
 
-const htmlTransformer = require("./html-transformer.js");
+const cssTransformer = require("./css-transformer.js");
 
 const file = `${__dirname}/../../examples/css/style.css`;
 const fileContent = fs.readFileSync(file, { encoding: "utf-8" });
 
 const ast = postcss.parse(fileContent);
-// console.log(ast.nodes);
+console.log(ast.nodes);
 // console.log(ast.nodes[0]);
 // console.log(ast.nodes[0].nodes);
 // console.log(ast.nodes[0].nodes[0]);
@@ -40,23 +40,49 @@ const predicate = (node) => {
 
 console.log(predicate(ast.nodes[0]));
 
-// const transformedAST = htmlTransformer({
-//   ast: postcss.parse(fileContent),
-//   transformers: [
-//     {
-//       nodeTargetPredicate: (node) => node.selector === ".red-sector-a" && node.attrs.class === "red",
-//       nodeTargetTransformer: (node) => {
-//         node.tag = "section";
-//         node.attrs.class = "blue";
-//         node.content[0] = {
-//           tag: "span",
-//           attrs: { class: "some-stylin" },
-//           content: "works!",
-//         };
-//       },
-//     },
-//   ],
-// });
+// ast.nodes[0].nodes
+//   .filter((n) => n.type === "decl")
+//   .forEach((n) => {
+//     console.log(n);
+//     console.log(n.prop);
+//     console.log(n.value);
+//   });
+
+const transformedAST = cssTransformer({
+  ast: postcss.parse(fileContent),
+  transformers: [
+    {
+      nodeTargetPredicate: (node) => {
+        return (
+          node.type === "rule" &&
+          node.selector === ".red-sector-a" &&
+          !!(() => {
+            return node?.nodes.filter((secondLevelNode) => {
+              return (
+                secondLevelNode.type === "rule" &&
+                secondLevelNode.selector === ".distant-early-warning" &&
+                secondLevelNode?.nodes.filter((thirdLevelNode) => {
+                  return (
+                    thirdLevelNode.type === "rule" && thirdLevelNode.selector === ".kid-gloves"
+                  );
+                })
+              );
+            });
+          })()
+        );
+      },
+      nodeTargetTransformer: (node) => {
+        let fontSizeDecl;
+        if (
+          undefined !==
+          (fontSizeDecl = node.nodes.find((n) => n.type === "decl" && n.prop === "font-size"))
+        ) {
+          fontSizeDecl.value = "2em";
+        }
+      },
+    },
+  ],
+});
 // const modifiedHTML = postHTMLRender(transformedAST, { closingSingleTag: "slash" });
 // console.log(modifiedHTML);
 // fs.writeFileSync(file, modifiedHTML, { encoding: "utf-8" });
