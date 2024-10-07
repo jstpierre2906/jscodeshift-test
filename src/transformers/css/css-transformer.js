6d690def9ec22224faec00f61936fe80d57288bf
jstpierre2906 <jstpierre2906@outlook.com>
2024-10-07 18:24:24 -0400

    adding node prop as legit locateAndTransform caller

--- src/transformers/css/css-transformer.js
+++ src/transformers/css/css-transformer.js
@@ -2,7 +2,7 @@ const locateAndTransform = require("../common-css-html/locate-and-transform.js")
 
 const cssTransformer = ({ ast, transformers }) => {
   const recurseThroughCSSTree = ({ node }) => {
-    if (node.selector) {
+    if (node.selector || node.prop) {
       locateAndTransform({ node, transformers });
     }
     if (Array.isArray(node?.nodes)) {
